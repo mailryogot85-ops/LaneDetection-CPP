c0a5881bc01a5e5579d98866748ca58c8ad2f243
Ryogo Takeuchi <mail.ryogo.t85@gmail.com>
2026-06-06 17:08:45 +0900

    first commit

---
+++ CMakeFiles/LaneDetection.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LaneDetection.dir/main.cpp.o"
+  "CMakeFiles/LaneDetection.dir/main.cpp.o.d"
+  "LaneDetection"
+  "LaneDetection.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LaneDetection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/LaneDetection.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for LaneDetection.
+# This may be replaced when dependencies are built.
